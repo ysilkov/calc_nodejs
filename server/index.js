54e9929e98efeaf34710bc68afdd7c6bbda87c68
ysilkov <ysilkov@ukr.net>
2022-08-22 00:22:19 +0300

    create

--- server/index.js
+++ server/index.js
@@ -1,12 +1,12 @@
 import express from "express";
 import cors from "cors";
-import router from "./router/app.js"
+import router from "./router/app.js";
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use('/api', router);
-const PORT = 5000; 
+app.use("/api", router);
+const PORT = 5000;
 
-app.listen(PORT, () => console.log('SERVER STARTED ON PORT ' + PORT))
+app.listen(PORT, () => console.log("SERVER STARTED ON PORT " + PORT));
