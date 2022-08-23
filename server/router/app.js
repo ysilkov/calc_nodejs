e5fbb4ac87b14da108d4c2feb59cadbdd0dd99e5
ysilkov <ysilkov@ukr.net>
2022-08-23 23:29:00 +0300

    update

--- server/router/app.js
+++ server/router/app.js
@@ -6,25 +6,44 @@ router.post("/add", (req, res) => {
   let a = parseInt(req.body.a);
   let b = parseInt(req.body.b);
   let ans = a + b;
-  res.send(JSON.stringify({ ans: ans }));
+  res.send(
+    JSON.stringify({
+      a: a,
+      b: b,
+      ans: ans,
+    })
+  );
 });
+
 router.post("/sub", (req, res) => {
   let a = parseInt(req.body.a);
   let b = parseInt(req.body.b);
   let ans = a / b;
-  res.send(JSON.stringify({ ans: ans }));
+  res.send(
+    JSON.stringify({
+      a: a,
+      b: b,
+      ans: ans,
+    })
+  );
 });
 router.post("/mul", (req, res) => {
   let a = parseInt(req.body.a);
   let b = parseInt(req.body.b);
   let ans = a * b;
-  res.send(JSON.stringify({ ans: ans }));
+  res.send(JSON.stringify({ a: a, b: b, ans: ans }));
 });
 router.post("/div", (req, res) => {
   let a = parseInt(req.body.a);
   let b = parseInt(req.body.b);
   let ans = a - b;
-  res.send(JSON.stringify({ ans: ans }));
+  res.send(
+    JSON.stringify({
+      a: a,
+      b: b,
+      ans: ans,
+    })
+  );
 });
 
 export default router;
